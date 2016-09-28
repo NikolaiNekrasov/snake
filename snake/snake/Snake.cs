9a11be95c98d0a95da199385f70d04cf3fa807ff
nick <nikolai.nekrasov@gmail.com>
2016-09-28 11:12:48 +0300

    питание змейки

--- snake/snake/Snake.cs
+++ snake/snake/Snake.cs
@@ -51,6 +51,19 @@ namespace snake
                 else if (key == ConsoleKey.UpArrow)
                     direction = Direction.UP;
             }
+
+        internal bool Eat(point food)
+        {
+            point head = GetNextPoint();
+            if (head.IsHit(food))
+            {
+                food.sym = head.sym;
+                pList.Add(food);
+                return true;
+            }
+            else
+                return false;
+        }
             
     }
 }
